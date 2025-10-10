71cdfedf845fbb7863f19f44f46424c1675e1bfa
fikalalif <fikalgans26@gmail.com>
2025-10-10 22:57:30 +0700

    fix package.json1

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -127,7 +127,11 @@ export default function ContentList({
         {items.map((post, index) => (
           <li
             key={index}
-            ref={(el) => (itemsRef.current[index] = el)}
+            ref={(el) => {
+              if (el) {
+                itemsRef.current[index] = el;
+              }
+            }}
             onMouseEnter={() => onMouseEnter(index)}
             className="list-item opacity-0"
           >
